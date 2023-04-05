7041d97478e7c95141be2d192dd657831c277830
narmuni123 <narmuni123@gmail.com>
2023-04-05 08:18:44 +0530

    Arrays and array functions

--- object-play.js
+++ object-play.js
@@ -7,3 +7,15 @@ const person = {
 }
 person.greet();
 console.log(person);
+
+
+// Playing with arrays
+const hobbies = ['Sports', 'Cooking'];
+
+for(let hobby in hobbies){
+    console.log(hobbies[hobby]);
+}
+
+console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+
+console.log(hobbies);
