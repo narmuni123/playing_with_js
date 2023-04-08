cd1634de60a23473f3f2d393a07b69e43acc3c0c
narmuni123 <narmuni123@gmail.com>
2023-04-08 22:39:18 +0530

    Async await

--- object-play.js
+++ object-play.js
@@ -1,63 +1,62 @@
 const person = {
-    name: 'User',
-    age: '25',
-    greet() {
-        console.log("Hi, " + this.name);
-    }
-}
+  name: "User",
+  age: "25",
+  greet() {
+    console.log("Hi, " + this.name);
+  },
+};
 
-// destructing 
+// destructing
 
-const printName = ({name}) => {
-    console.log(name);
-}
+const printName = ({ name }) => {
+  console.log(name);
+};
 
-const {name, age} = person;
+const { name, age } = person;
 
-console.log(name,age);
-
-
+console.log(name, age);
 
 printName(person);
 
 person.greet();
 console.log(person);
 
-
 // Playing with arrays
-const hobbies = ['Sports', 'Cooking'];
+const hobbies = ["Sports", "Cooking"];
 
-for(let hobby in hobbies){
-    console.log(hobbies[hobby]);
+for (let hobby in hobbies) {
+  console.log(hobbies[hobby]);
 }
 
 // spread operator
 const copiedArray = [...hobbies];
-console.log(copiedArray); 
+console.log(copiedArray);
 
 // Rest operator
 
 const toArray = (...args) => {
-    return args;
-}
+  return args;
+};
 
-console.log(toArray(1,2,3,4,5));
+console.log(toArray(1, 2, 3, 4, 5));
 
-//// 
-console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+////
+console.log(hobbies.map((hobby) => "Hobby: " + hobby));
 
 console.log(hobbies);
 
-const fetchData = callback => {
+const fetchData = (callback) => {
+  const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        callback('Done');
+      resolve("Done");
     }, 1500);
+  });
+  return promise;
 };
 
 setTimeout(() => {
-    console.log('Timer is done!!');
-    fetchData(text => {
-        console.log('Done');
-    });
-    console.log("User")
-}, 4000);
+  console.log("Timer is done!!");
+  fetchData().then((done) => {
+    console.log(text);
+  });
+}, 2000);
