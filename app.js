4c80ad12f536d6db0fdd427632d9d2515d393e82
narmuni123 <narmuni123@gmail.com>
2023-04-18 17:17:35 +0530

    redirecting

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require("http");
-
+const fs = require("fs"); 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if(url === '/') {
     res.write("<html>");
     res.write("<head><title>Enter Nessage</title></head>");
@@ -9,7 +10,13 @@ const server = http.createServer((req, res) => {
     res.write("</html>");
     return res.end();
   }
-  // process.exit();
+
+  if(url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader('location', '/');
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
