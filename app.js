8617a5c6d6c3d8b9487948c02fe2225f3ac54bca
narmuni123 <narmuni123@gmail.com>
2023-04-20 08:38:36 +0530

    Blocking and non-blocking code

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 const http = require("http");
-const fs = require("fs"); 
+const fs = require("fs");
 const { log } = require("console");
 const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
-  if(url === '/') {
+  if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter Nessage</title></head>");
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button><button type="submit">Send</button></form></body>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button><button type="submit">Send</button></form></body>'
+    );
     res.write("</html>");
     return res.end();
   }
 
-  if(url === "/message" && method === "POST") {
+  if (url === "/message" && method === "POST") {
     const body = [];
 
-    req.on('data', (chunk) => {
+    req.on("data", (chunk) => {
       console.log(chunk);
       body.push(chunk);
     });
 
-    req.on('end', () => {
+    return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFileSync("message.txt", message);
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("location", "/");
+        return res.end();
+      });
     });
-  
-    res.statusCode = 302;
-    res.setHeader('location', '/');
-    return res.end();
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
